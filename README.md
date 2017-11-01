114b1d91ac9645b092fb9ef35a0dc1c27472f575
abhudev <abhipathak97@gmail.com>
2017-11-01 21:02:12 +0530

    Comments and README

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# FBS
-Develop complete Frame Based System (FBS) for one of the applications (University or Hospital).
+# Frame Based System
+
+This is an implementation of a frame based system for represetation of data pertaining to a university/hospital. The example we have chosen is that of a university.
+
+# How to run
+
+You should have Prolog (say swi-Prolog) installed. Open a terminal at the location of the source code, and enter:
+
+	swipl frames.pl
+
+Then, you can query the slot values of frames, insert new frames, delete entire subtrees of frames or update the slot values of frames.
+
+To insert a new frame with name Fname, parent Pname and list of slot-value pairs L, give the command:
+	
+	:- insertframe(Fname, Pname, L).
+
+If the frame already exists, or the parent does not exist, an error will occur.
+
+To delete a frame and its subtree, give the command:
+
+	:- delete(Fname).
+
+To update slot S of a frame F with value V, give the command:
+
+	:- updateframe(F, S, V).
+
+The slot can be even that which belongs to an ancestor (in the network of frames) of the frame F.
+
+To find the value V of a slot S for frame F, give the command:
+	
+	:- find(F, S, V).
+
+If the frame or the required slot does not exist, an error is returned.
--- frames.pl
+++ frames.pl
@@ -1,7 +1,6 @@
 :- dynamic frame/2.
 
-
-%% network of Frames
+%% A network of Frames for a university
 frame(university, [(phone,(default, 011686971)),(address,(default, iitDelhi))]).
 frame(department, [(a_part_of,university),(programme,(btech, mtech, ph_d))]).
 frame(hostel, [(a_part_of,university), (room,(default, 100))]).
@@ -11,38 +10,41 @@ frame(science_faculty, [(ako,faculty),(qual,(default, msc))]).
 frame(renuka, [(is_a,science_faculty),(qual,ph_d),(age,45),(address,janakpuri)]).
 
 
-%% Module to insert a frame in NOF with all slot values filled up
+%% Module to insert a frame in network of frames with all slot values filled up
 %% Fname : Frame Name
-%% Pname : Parent Name
+%% Pname : Parent Name (Fname will be ako Pname, is_a Pname, or a_part_of Pname)
 
-insertframe(Fname,_,_):-frame(Fname,_),!, 
+%% Case 1: Fname frame alreay present
+insertframe(Fname,_,_):-frame(Fname,_),!,
 						write("\n Error- Frame: "),write(Fname),write(" Already Exists"),nl.
+%% Case 2: Fname frame added with parent Pname (Pname already exists in the network of frames)
 insertframe(Fname,Pname,List):-frame(Pname,_),assertz(frame(Fname,List)).
-insertframe(_,Pname,_):-write("\n Error - Invalid Parent Name: "),write(Pname),nl. 
-
+%% Case 3: Fname frame can't be added as the Pname frame is not present
+insertframe(_,Pname,_):-write("\n Error - Invalid Parent Name: "),write(Pname),nl.
 
 %% Module to delete a frame from NOF
-%% Deletes the whole subtree below the frame 
+%% Deletes all the subtrees below the frame 
 deleteframe(Fname):-frame(Fname,_), frame(Y,[(a_part_of,Fname)|_]),deleteframe(Y).
 deleteframe(Fname):-frame(Fname,_), frame(Y,[(ako,Fname)|_])      ,deleteframe(Y).
 deleteframe(Fname):-frame(Fname,_), frame(Y,[(is_a,Fname)|_])     ,deleteframe(Y).
 deleteframe(Fname):-frame(Fname,Z), write("Deleting "),write(Fname),nl, retract(frame(Fname,Z)).
 
 
-%% Update the value of the slot for a given frame
+%% Helper predicate for 'updateframe'
 update([(Slot,_)|L],Slot,Newval,[(Slot,Newval)|L]).
 update([H|L],Slot,Newval,[H|NewL]):-update(L,Slot,Newval,NewL).
-
+%% Update the value of the slot for a given frame using 'updateframe'
 updateframe(Fname,Slot,Val):- 	frame(Fname,Z),!,update(Z,Slot,Val,NewZ),
 								retract(frame(Fname,Z)),assertz(frame(Fname,NewZ)),
 								write("\nUpdated"),nl,!.
 updateframe(Fname,_,_):-write("\n Error - Invalid Frame Name: "),write(Fname),nl. 
 
 
-%% Module to query FBS
+%% Module to query FBS by searching for the appropriate slot value recursively in the network of frames
+%% Helper predicate for 'find'
 search([(Slot,Val)|_],Slot,Val):-!.
 search([_|List],Slot,Val):-search(List,Slot,Val).
-
+%% Use 'find' to get the slot values for frames
 find(X, Slot,Val) :- frame(X, Z) , 					search(Z, Slot,Val),!.
 find(X, Slot,Val) :- frame(X, [(is_a,Z)|_]) , 		find(Z, Slot,Val),!.
 find(X, Slot,Val) :- frame(X, [(ako,Z)|_]) , 		find(Z, Slot,Val),!.
